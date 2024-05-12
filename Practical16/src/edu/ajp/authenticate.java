d84604fb899bf5cf5ea9efb3abc858f6db122028
Arun Parihar <arun.parihar@gmail.com>
2024-05-12 20:02:53 +0100

    lab16 cleanup

--- Practical16/src/edu/ajp/authenticate.java
+++ Practical16/src/edu/ajp/authenticate.java
@@ -12,25 +12,24 @@ import javassist.SerialVersionUID;
 public class authenticate extends ActionSupport {
 	
 	private static final long serialVersionUID = 1L;
+
 	private String user;
 	public String getUser() {
 		return user;
 	}
-
 	public void setUser(String user) {
 		this.user = user;
 	}
 
+	private String pass;
 	public String getPass() {
 		return pass;
 	}
-
 	public void setPass(String pass) {
 		this.pass = pass;
 	}
 
-	private String pass;
-	
+
 	public String execute() throws Exception {
 		DriverManager.registerDriver(new com.mysql.jdbc.Driver());
 		Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/ajp","ajp","ajp");
